d0ee97d26466dba3f476815486e97c784cbe3152
Piotr FLEURY <piotrfleury@macbook-pro-de-piotr-2-1.home>
2021-12-24 16:33:29 +0100

    handle user input

---
+++ discoveringswiftui/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  discoveringswiftui
+//
+//  Created by Piotr FLEURY on 24/12/2021.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
