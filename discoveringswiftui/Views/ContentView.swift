39691274bade84a9bf566bf29cc2ecacb9dcf13f
Piotr FLEURY <piotrfleury@macbook-pro-de-piotr-2-1.home>
2021-12-24 15:52:47 +0100

    first commit

---
+++ discoveringswiftui/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  discoveringswiftui
+//
+//  Created by Piotr FLEURY on 24/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ discoveringswiftui/discoveringswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  discoveringswiftuiApp.swift
+//  discoveringswiftui
+//
+//  Created by Piotr FLEURY on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct discoveringswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
